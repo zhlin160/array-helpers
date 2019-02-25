d08bc4c1b18724a0807b109381e6810cf72419cd
小毛竹子- <zhlin163@sina.com>
2019-02-25 17:28:50 +0800

    'add'

---
+++ demo/demo.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zh
+ * Date: 2019/2/25
+ * Time: 16:41
+ */
+require_once "../vendor/autoload.php";
+
+use zh\helpers\ArrayHelper;
+
+$array = [
+         ['id' => '123', 'data' => 'abc', 'device' => 'laptop'],
+         ['id' => '345', 'data' => 'def', 'device' => 'tablet'],
+         ['id' => '345', 'data' => 'hgi', 'device' => 'smartphone'],
+];
+
+//$result = ArrayHelper::index($array, null, 'id');
+$result = ArrayHelper::getValue($array, 'id');
+var_dump($result);
---
+++ src/ArrayHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace zh\helpers;
+
+/**
+ * ArrayHelper provides additional array functionality that you can use in your
+ * application.
+ *
+ * For more details and usage information on ArrayHelper, see the [guide article on array helpers](guide:helper-array).
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class ArrayHelper extends BaseArrayHelper
+{
+}
